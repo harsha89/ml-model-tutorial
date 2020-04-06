0f0293413544f26c6407d599f954abfb4772c01b
harsha89 <harsz89@gmail.com>
2020-04-06 21:26:52 +1000

    initial commit

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+import pandas as pd
+import joblib
+
+app = Flask(__name__)
+
+@app.route("/predict", methods=['POST'])
+def do_prediction():
+    json = request.get_json()
+    model = joblib.load('model/rf_model.pkl')
+    df = pd.DataFrame(json, index=[0])
+
+    from sklearn.preprocessing import StandardScaler
+    scaler = StandardScaler()
+    scaler.fit(df)
+
+    df_x_scaled = scaler.transform(df)
+
+    df_x_scaled = pd.DataFrame(df_x_scaled, columns=df.columns)
+    y_predict = model.predict(df_x_scaled)
+
+    result = {"Predicted House Price" : y_predict[0]}
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
